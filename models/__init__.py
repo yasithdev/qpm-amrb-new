e073d9100e24b81d18c9c93d4b76ac8146f7def9
Yasith Jayawardana <mail@yasith.dev>
2022-09-16 14:53:41 -0400

    load model to device before summary

--- models/__init__.py
+++ models/__init__.py
@@ -9,6 +9,7 @@ def get_model_optimizer_and_loops(
     config: Config,
     experiment_path: str,
 ) -> Tuple[torch.nn.Module, torch.optim.Optimizer, Callable, Callable]:
+    
     # load requested module, if available
     input_size = config.image_chw
     if config.model_name == "nf":
@@ -26,8 +27,12 @@ def get_model_optimizer_and_loops(
         from .model_bnn import load_model_and_optimizer, test_model, train_model
     else:
         raise ImportError(f"Model not found: {config.model_name}")
-    # return model, optimizer, training loop, and testing loop
+    
+    # load model and optimizer to device
     model, optimizer = load_model_and_optimizer(config, experiment_path)
+    model = model.to(config.device)
+    
+    # print model summary
     if type(model) == torch.nn.ModuleDict:
             torchsummary.summary(
                 model["encoder"],
@@ -42,4 +47,6 @@ def get_model_optimizer_and_loops(
             batch_size=config.batch_size,
             device=config.device
         )
+
+    # return model, optimizer, training loop, and testing loop
     return model, optimizer, train_model, test_model
