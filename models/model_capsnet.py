fix gradient vanishing caused by group norm

models/model_capsnet.py
@@ -107,6 +107,7 @@ def load_model_and_optimizer(
         }
     )
 
+    # set up optimizer
     optim_config = {"params": model.parameters(), "lr": config.optim_lr}
     optim = torch.optim.AdamW(**optim_config)
 
@@ -200,25 +201,29 @@ def step_model(
             gather_samples(samples, x, y, x_z, y_z)
 
             # calculate loss
-            # classification_loss = margin_loss(y_z, y) - replaced with evidential loss
-            classification_loss = edl_loss(y_z, y, epoch).mean()
+            # L_y_z = margin_loss(y_z, y) - replaced with evidential loss
+            L_y_z = edl_loss(y_z, y, epoch)
             mask = pY.argmax(-1).eq(y.argmax(-1)).nonzero()
-            reconstruction_loss = F.mse_loss(x_z[mask], x[mask])
-            l = 0.9
-            minibatch_loss = l * classification_loss + (1 - l) * reconstruction_loss
+            L_x_z = (x_z[mask] - x[mask]).pow(2).flatten(1).mean(-1)
+            l = 0.999
+            L_minibatch = (l * L_y_z + (1 - l) * L_x_z).mean()
 
             # backward pass
             if optim:
                 (optim_gd,) = optim
                 optim_gd.zero_grad()
-                minibatch_loss.backward()
+                L_minibatch.backward()
                 optim_gd.step()
 
             # accumulate sum loss
-            sum_loss += minibatch_loss.item() * config.batch_size
+            sum_loss += L_minibatch.item() * config.batch_size
 
             # logging
-            log_stats = {"Loss(mb)": f"{minibatch_loss.item():.4f}"}
+            log_stats = {
+                "Loss(mb)": f"{L_minibatch:.4f}",
+                "Loss(x)": f"{L_x_z.mean():.4f}",
+                "Loss(y)": f"{L_y_z.mean():.4f}",
+            }
             iterable.set_postfix(log_stats)
 
     # post-step

models/model_flow_nll.py
@@ -136,10 +136,8 @@ def load_model_and_optimizer(
     )
 
     # set up optimizer
-    optim = torch.optim.AdamW(
-        params=model.parameters(),
-        lr=config.optim_lr,
-    )
+    optim_config = {"params": model.parameters(), "lr": config.optim_lr}
+    optim = torch.optim.AdamW(**optim_config)
 
     return model, (optim,)
 
@@ -241,11 +239,11 @@ def step_model(
             # manifold losses
             L_z = -dist_z.log_prob(z_x)
             L_v = -dist_v.log_prob(v_x)
-            L_m = (x - x_z).pow(2).flatten(1).sum(-1)
+            L_x = (x - x_z).pow(2).flatten(1).sum(-1)
 
             # classification losses
-            # L_y_x = F.cross_entropy(y_x, y) - used EDL alternative
-            L_y_x = edl_loss(y_z, y, epoch)
+            # L_y_z = F.cross_entropy(y_x, y) - used EDL alternative
+            L_y_z = edl_loss(y_z, y, epoch)
 
             # accumulate predictions
             y_true.extend(y.detach().argmax(-1).cpu().numpy())
@@ -261,8 +259,8 @@ def step_model(
 
             # compute minibatch loss
             ß = 1e-3
-            # L_minibatch = L_z.mean() + (ß * L_v.mean()) + L_m.mean() + L_y_x.mean() # Σ_i[Σ_n(L_{n,i})/N]
-            L_minibatch = (L_z + (ß * L_v) + L_m + L_y_x).mean()  # Σ_n[Σ_i(L_{n,i})]/N
+            # L_minibatch = L_z.mean() + (ß * L_v.mean()) + L_m.mean() + L_y_z.mean() # Σ_i[Σ_n(L_{n,i})/N]
+            L_minibatch = (L_z + (ß * L_v) + L_x + L_y_z).mean()  # Σ_n[Σ_i(L_{n,i})]/N
 
             # backward pass (if optimizer is given)
             if optim:
@@ -277,10 +275,10 @@ def step_model(
             # logging
             log_stats = {
                 "Loss(mb)": f"{L_minibatch:.4f}",
-                "Loss(z)": f"{L_z.mean():.4f}",
+                "Loss(x)": f"{L_x.mean():.4f}",
+                "Loss(y)": f"{L_y_z.mean():.4f}",
                 "Loss(v)": f"{L_v.mean():.4f}",
-                "Loss(m)": f"{L_m.mean():.4f}",
-                "Loss(y)": f"{L_y_x.mean():.4f}",
+                "Loss(z)": f"{L_z.mean():.4f}",
             }
             iterable.set_postfix(log_stats)
 

models/resnet/__init__.py
@@ -31,7 +31,7 @@ def get_encoder(
             stride=2,
         ),
         # (B, d/4, h/2, w/2)
-        torch.nn.GroupNorm(d // 4, d // 4),
+        torch.nn.GroupNorm(1, d // 4),
         # (B, d/4, h/2, w/2)
         torch.nn.ReLU(),
         # (B, d/4, h/2, w/2)
@@ -74,7 +74,7 @@ def get_encoder(
             kernel_size=(h // 8, w // 8),
         ),
         # (B, d, 1, 1)
-        torch.nn.GroupNorm(d, d),
+        torch.nn.GroupNorm(1, d),
         # (B, d, 1, 1)
     )
 

models/resnet/residual_block.py
@@ -49,7 +49,7 @@ class ResidualBlock(torch.nn.Module):
                 stride=1,
                 bias=False,
             ),
-            self.Norm(self.hidden_channels, self.hidden_channels),  # type: ignore
+            self.Norm(1, self.hidden_channels),  # type: ignore
         )
 
         args = {}
@@ -66,7 +66,7 @@ class ResidualBlock(torch.nn.Module):
                 bias=False,
                 **args,
             ),
-            self.Norm(self.hidden_channels, self.hidden_channels),  # type: ignore
+            self.Norm(1, self.hidden_channels),  # type: ignore
         )
 
         # 1x1 convolution layer
@@ -78,7 +78,7 @@ class ResidualBlock(torch.nn.Module):
                 stride=1,
                 bias=False,
             ),
-            self.Norm(self.out_channels, self.out_channels),  # type: ignore
+            self.Norm(1, self.out_channels),  # type: ignore
         )
 
         # if stride != 1, match identity to x using stridexstride convolution
@@ -91,7 +91,7 @@ class ResidualBlock(torch.nn.Module):
                     stride=self.stride,
                     bias=False,
                 ),
-                self.Norm(self.out_channels, self.out_channels),  # type: ignore
+                self.Norm(1, self.out_channels),  # type: ignore
             )
 
     def forward(