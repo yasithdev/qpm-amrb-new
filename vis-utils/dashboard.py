cd1034667a000af963346c3e00ce9f0889863411
Ramith Hettiarachchi <im@ramith.fyi>
2023-06-21 16:46:35 -0400

    cache dataloaders and model load

--- vis-utils/dashboard.py
+++ vis-utils/dashboard.py
@@ -23,41 +23,46 @@ os.environ["CV_MODE"] = "k-fold"
 os.environ["CV_FOLDS"] = "10"
 os.environ["DATASET_NAME"] = "AMRB2_species.4"
 
-
-
-config = load_config()
-config.image_chw = get_dataset_chw(
-        dataset_name=config.dataset_name,
-)
-
-config.dataset_info = get_dataset_info(
-    dataset_name=config.dataset_name,
-    data_root=config.data_dir,
-    cv_mode=config.cv_mode,
-)
-
-train_loader, test_loader = get_dataset_loaders(
-        dataset_name = config.dataset_name,
-        batch_size_train = 32,
-        batch_size_test = 32,
-        data_root = "/n/holyscratch01/wadduwage_lab/yasith/datasets",
-        cv_k = config.cv_k,
-        cv_folds = config.cv_folds,
-        cv_mode = config.cv_mode,
+@st.cache_data
+def load_stuff():
+    config = load_config()
+    config.image_chw = get_dataset_chw(
+            dataset_name=config.dataset_name,
     )
 
-PATH = "/n/home12/ramith/experiments/ood_flows/AMRB2_species/k-fold-N10-K4-M50_1-17-53.097923/rescaps_model_e100.pth"
+    config.dataset_info = get_dataset_info(
+        dataset_name=config.dataset_name,
+        data_root=config.data_dir,
+        cv_mode=config.cv_mode,
+    )
 
-device = torch.device('cpu')
-weights = torch.load(PATH, map_location=device)
+    train_loader, test_loader = get_dataset_loaders(
+            dataset_name = config.dataset_name,
+            batch_size_train = 32,
+            batch_size_test = 32,
+            data_root = "/Users/ramith_1/Datasets/",
+            cv_k = config.cv_k,
+            cv_folds = config.cv_folds,
+            cv_mode = config.cv_mode,
+        )
 
-model, _, _  = get_model_optimizer_and_step(config)
-model.load_state_dict(weights)
-model.to(device)
+    PATH = "../experiments/ood_flows/AMRB2_species/53.097923/rescaps_model_e100.pth"
 
-encoder = model["encoder"]
-classifier = model["classifier"]
-decoder = model["decoder"]
+    device = torch.device('cpu')
+    weights = torch.load(PATH, map_location=device)
+
+    model, _, _  = get_model_optimizer_and_step(config)
+    model.load_state_dict(weights)
+    model.to(device)
+
+    encoder = model["encoder"]
+    classifier = model["classifier"]
+    decoder = model["decoder"]
+    
+    return encoder, classifier, decoder, train_loader, test_loader
+
+
+encoder, classifier, decoder, train_loader, test_loader = load_stuff()
 
 st.sidebar.header("Settings")
 mode = st.sidebar.selectbox('Choose mode', ['train', 'test'])
@@ -129,26 +134,26 @@ class_means = []
 # plt.ylabel('Variance')
 # plt.savefig("varience_plot.png")
 
-with st.expander("Plots"):  # Collapsible section for plots
+# with st.expander("Plots"):  # Collapsible section for plots
 
-    for i in range(0,4):
-        mean_vector = all_target_prototypes[all_y.argmax(dim = 1) == i].mean(axis = 0)
-        class_means.append(mean_vector)
-        fig = plt.figure(figsize=(3,3))
-        plt.imshow(all_target_prototypes[all_y.argmax(dim = 1) == i].detach().cpu())
-        st.pyplot(fig)  # Here we use Streamlit's function to show the plot
+for i in range(0,4):
+    mean_vector = all_target_prototypes[all_y.argmax(dim = 1) == i].mean(axis = 0)
+    class_means.append(mean_vector)
+    fig = plt.figure(figsize=(3,3))
+    plt.imshow(all_target_prototypes[all_y.argmax(dim = 1) == i].detach().cpu())
+    st.pyplot(fig)  # Here we use Streamlit's function to show the plot
 
 
-    class_reps = torch.cat(class_means, dim=1).T
+class_reps = torch.cat(class_means, dim=1).T
 
-    fig2 = plt.figure(figsize=(10,10))
-    plt.imshow(class_reps)
-    st.pyplot(fig2)
+fig2 = plt.figure(figsize=(10,10))
+plt.imshow(class_reps)
+st.pyplot(fig2)
 
-    pos_variance = torch.var(class_reps, dim = 0).reshape(50,1)
+pos_variance = torch.var(class_reps, dim = 0).reshape(50,1)
 
-    fig3 = plt.figure(figsize=(24,4))
-    plt.bar(range(pos_variance.shape[0]),pos_variance)
-    plt.xlabel('Position')
-    plt.ylabel('Variance')
-    st.pyplot(fig3)
+fig3 = plt.figure(figsize=(24,4))
+plt.bar(range(pos_variance.shape[0]),pos_variance)
+plt.xlabel('Position')
+plt.ylabel('Variance')
+st.pyplot(fig3)
